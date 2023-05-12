a6fcd94fafb9a0954eba4b9d172521823c3be6ed
MomtajHossainMow <momtajhossain2001@gamil.com>
2023-05-13 02:04:52 +0600

    Android Project

--- app/src/main/java/com/example/patientsmedicalhistory/Admin_Control_Admin.java
+++ app/src/main/java/com/example/patientsmedicalhistory/Admin_Control_Admin.java
@@ -9,22 +9,25 @@ import android.widget.Button;
 
 public class Admin_Control_Admin extends AppCompatActivity {
     private Button btn_create_admin, btn_adminlist, btn_modify;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_control_admin);
 
-        //Create New Admin ID Button----------------------------------------------------------------
+        // Create New Admin ID
+        // Button----------------------------------------------------------------
         btn_create_admin = findViewById(R.id.btn_create_AID);
         btn_create_admin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent(Admin_Control_Admin.this, SignUp_of_Admin.class);
                 startActivity(intent);
             }
         });
-        //Modify Admin Data-------------------------------------------------------------------------
+        // Modify Admin
+        // Data-------------------------------------------------------------------------
         btn_modify = findViewById(R.id.btn_modify_aid);
         btn_modify.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +37,8 @@ public class Admin_Control_Admin extends AppCompatActivity {
             }
         });
 
-        //Check Admin List--------------------------------------------------------------------------
+        // Check Admin
+        // List--------------------------------------------------------------------------
         btn_adminlist = findViewById(R.id.btn_admin_list);
         btn_adminlist.setOnClickListener(new View.OnClickListener() {
             @Override
